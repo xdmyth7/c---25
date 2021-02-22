e3a8515b4da80ecc2e76e3d99023706645190599
xdmyth7 <77282774+xdmyth7@users.noreply.github.com>
2021-02-22 16:01:49 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,10 @@
+class Log extends BaseClass {
+    constructor(x, y, height, angle) {
+      super(x,y,20,height,angle);
+     this.image = loadImage("spirites/wood2.png");
+      Matter.Body.setAngle(this.body, angle);
+      
+    }
+    
+  }
+  
